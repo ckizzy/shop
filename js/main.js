219ad8eeef62c02609919f6c8fc0690f4b204840
Anastazija Veselinović <ckizianna@Anastazijas-MacBook-Air.local>
2019-01-14 23:46:49 +0100

    2.commit - finished index.html template

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-$(".back-to-top").click(function () {
-  $("html, body").animate({scrollTop: 0}, 1000);
+$.ajax({
+  url: "https://raw.githubusercontent.com/Danilovesovic/shop/master/shop.json",
+  dataType: "json"
+}).done(function(res) {
+  let dataBase = res;
+  //create html
+  let template = document.querySelector('[type="template"]').innerHTML;
+  let mainRow = document.querySelector("#insertTemplate");
+  console.log(template);
+
+  let text = "";
+  for (let i = 0; i < dataBase.length; i++) {
+    text += template
+      .replace(/{{imgSrc}}/gi, dataBase[i].imgSrc)
+      .replace(/{{productTitle}}/gi, dataBase[i].productTitle)
+      .replace(/{{model}}/gi, dataBase[i].model)
+      .replace(/{{price}}/gi, dataBase[i].price);
+  }
+  mainRow.innerHTML = text;
+});
+
+//bojanov front
+
+$(".back-to-top").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, 1000);
 });
